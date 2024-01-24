96d5e32b5627a17b0795085a36dee6c5228f5d8f
Fremah Agyemang <captainfremah66@gmail.com>
2024-01-24 18:13:56 +0000

    Add API to retireve list of nearby stores

--- stores/serializers.py
+++ stores/serializers.py
@@ -1,3 +1,25 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from stores.models import Store
+
+class StoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Store
+        fields = [
+            'id', 'created_at', 'name', 'rating', 'store_type', 'opening_hour',
+            'closing_hour', 'city', 'latitude', 'longitude', 'location', 'address', 'phone',
+        ]
+
+class NearbyStoreSerializer(StoreSerializer):
+
+    distance = serializers.SerializerMethodField()
+
+    def get_distance(self, instance):
+        return instance.distance.mi if instance else 'N/A'
+    
+    class Meta:
+        model = Store
+        fields = [
+            'id', 'name', 'rating', 'opening_hour', 'closing_hour', 'store_type', 
+            'address', 'latitude', 'longitude', 'distance',
+        ]
