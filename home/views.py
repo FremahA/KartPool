bf504ebfa877e245b3891889904db8387845c759
Fremah Agyemang <captainfremah66@gmail.com>
2024-01-24 18:23:06 +0000

    Add API to serve an HTML page

--- home/views.py
+++ home/views.py
@@ -5,4 +5,10 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
 
-# Create your views here.
+class HomePage(viewsets.GenericViewSet):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'home/index.html'
+
+    def list(self, request):
+        username = self.request.query_params.get('username')
+        return Response({'username': username})
