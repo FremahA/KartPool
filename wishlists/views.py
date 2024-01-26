b7a0cc99e9c606c7ab68422e8db8cd7da15457ce
Fremah Agyemang <captainfremah66@gmail.com>
2024-01-26 04:54:53 +0000

    Add wishlists app

---
+++ wishlists/views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets  
+from rest_framework.response import Response
+from .models import Wishlist  
+from .serializers import WishlistSerializer
+
+class WishlistView(viewsets.ModelViewSet):
+    queryset = Wishlist.objects.all()
+    serializer_class = WishlistSerializer
